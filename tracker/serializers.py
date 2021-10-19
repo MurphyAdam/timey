127469c8fd1f462956838adaeda539dc67de69a0
Adam Clark <elm.majidi@gmail.com>
2021-10-19 22:45:54 +0100

    added docstr to serializers. Override TrackerWriteSerializer' to_representation

--- tracker/serializers.py
+++ tracker/serializers.py
@@ -7,6 +7,10 @@ from .models import Tracker
 
 
 class TrackerSerializer(serializers.ModelSerializer):
+    """
+    This serializer is used for READ only operations on the Tracker model.
+    """
+
     user = UserSerializer(read_only=True, many=False)
     project = ProjectReadSerializer(read_only=True, many=False)
     url = serializers.HyperlinkedIdentityField(
@@ -22,6 +26,10 @@ class TrackerSerializer(serializers.ModelSerializer):
 
 
 class TrackerWriteSerializer(serializers.ModelSerializer):
+    """
+    This serializer is used for CREATE, UPDATE only operations on the Tracker model.
+    """
+
     user = serializers.PrimaryKeyRelatedField(
         queryset=User.objects.all(), many=False)
     project = serializers.PrimaryKeyRelatedField(
@@ -35,3 +43,18 @@ class TrackerWriteSerializer(serializers.ModelSerializer):
         read_only_fields = ('seconds_paused',
                             'pause_time', 'hours',
                             'date_updated', 'total_hours', )
+
+    def to_representation(self, instance):
+        """
+        We use to_representation because we want to recieve data in one format, 
+        but return it in another.
+        This class (TrackerWriteSerializer) is mainly for creating/updating Tracker entries, 
+        but it is also used by default to return the created/ updated record(s), 
+        which is indesirable since for example we will be getting a project id, 
+        and a user id as a JSON response instead of a serialized user/ project object.
+        """
+        # context will be needed TrackerSerializer url field which is using
+        # the HyperlinkedIdentityField
+        request = self.context['request']
+        serializer = TrackerSerializer(instance, context={'request': request})
+        return serializer.data
