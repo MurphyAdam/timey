684ee4a8fc7a102f9d47c802ff6bad1c2301d433
Adam Clark <elm.majidi@gmail.com>
2021-10-19 22:47:39 +0100

    added docstr to serializers. + ProjectReadSerializer's assigned_to field is now returning array[users_ids] instead of array[users_objects]

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
-from accounts.serializers import UserSerializer
 from accounts.models import User
 from .models import Project
 
 
 class ProjectReadSerializer(serializers.ModelSerializer):
-    assigned_to = UserSerializer(many=True, read_only=False)
+    """
+    This serializer is used for READ only operations on the Project model.
+    """
+
+    # We only return the list of user ids who have been assigned
+    # this project, instead of serialized user objects
+    assigned_to = serializers.PrimaryKeyRelatedField(
+        many=True, read_only=True)
     url = serializers.HyperlinkedIdentityField(
         view_name="project-detail", read_only=True
     )
@@ -19,6 +25,12 @@ class ProjectReadSerializer(serializers.ModelSerializer):
 
 
 class ProjectWriteSerializer(serializers.ModelSerializer):
+    """
+    This serializer is used for CREATE, UPDATE operations on the Project model.
+    """
+
+    # We receive list of user ids (ids[int] <= 0) by which we assign
+    # users to a project
     assigned_to = serializers.PrimaryKeyRelatedField(
         queryset=User.objects.all(), many=True)
 
