c6e03315dc2e12621fa8c96ac292da040889665d
Adam Clark <elm.majidi@gmail.com>
2021-10-19 18:32:35 +0100

    created constants for Project model

---
+++ projects/constants.py
@@ -0,0 +1,8 @@
+status = (
+    ('to-do', 'To Do'),
+    ('progress', 'Progress'),
+    ('done', 'Done'),
+)
+
+PRIORITY_CHOICES = (("unset", "Unset"), ("Low", "Low"),
+                    ("Medium", "Medium"), ("High", "High"))
