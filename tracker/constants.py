aabf94b2c5e4f4d852e564e84a634d4bcf0c56f4
Adam Clark <elm.majidi@gmail.com>
2021-10-19 18:35:13 +0100

    created constants for Tracker model

---
+++ tracker/constants.py
@@ -0,0 +1,7 @@
+STATUSES = (
+    ('UNVERIFIED', 'Unverified'),
+    ('VERIFIED', 'Verified'),
+    ('APPROVED', 'Approved'),
+    ('INVOICED', 'Invoiced'),
+    ('NOT_INVOICED', 'Not Invoiced'),
+)
