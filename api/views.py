722575231f1bb8a6c1fa5996349ea63a648d830f
Adam Clark <elm.majidi@gmail.com>
2021-10-20 15:40:51 +0100

    updated available endpoints

--- api/views.py
+++ api/views.py
@@ -9,7 +9,10 @@ def api_root(request, format=None):
     This is the api root view. It provides the available views.
     """
     return Response({
-                    'users': reverse('user-list', request=request, format=format),
-                    'projects': reverse('projects-list', request=request, format=format),
-                    'tracker': reverse('tracker-list', request=request, format=format),
+                    'Users': reverse('user-list', request=request, format=format),
+                    'Projects': reverse('projects-list', request=request, format=format),
+                    'Tracker': reverse('tracker-list', request=request, format=format),
+                    'My Tracker Entries': reverse('my-tracker-list', request=request, format=format),
+                    'Tracker timer-state': reverse('timer-state-tracker-list',
+                                                   request=request, format=format),
                     })
