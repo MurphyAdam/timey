f3a4e4d680404b9c8c2718b8676945d290183c3b
Adam Clark <elm.majidi@gmail.com>
2021-10-20 15:37:14 +0100

    created MyTrackerViewSet and TrackerTimerStateViewSet

--- tracker/views.py
+++ tracker/views.py
@@ -1,14 +1,36 @@
-from .serializers import TrackerSerializer, TrackerWriteSerializer
-from timey.permissions import IsOwnerOrAdmin
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .serializers import TrackerSerializer, TrackerWriteSerializer, PauseTrackerSerializer
+from timey.permissions import IsOwnerOrAdmin
 from .models import Tracker
 
 
 class TrackerViewSet(viewsets.ModelViewSet):
     queryset = Tracker.objects.all()
-    permission_classes = [IsOwnerOrAdmin]
+    permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
 
     def get_serializer_class(self):
         if self.request.method in ['POST', 'PUT', 'PATCH']:
             return TrackerWriteSerializer
+        return TrackerSerializer
+
+
+class MyTrackerViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.tracker_entries.all()
+
+    def get_serializer_class(self):
+        return TrackerSerializer
+
+
+class TrackerTimerStateViewSet(viewsets.ModelViewSet):
+    queryset = Tracker.objects.all()
+    permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
+
+    def get_serializer_class(self):
+        if self.request.method in ['PUT']:
+            return PauseTrackerSerializer
         return TrackerSerializer
