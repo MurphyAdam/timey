eff54e49819505072a6e1675162b30d633fd8c8c
Adam Clark <elm.majidi@gmail.com>
2021-10-20 19:16:48 +0100

    updated ProjectViewSet

--- projects/views.py
+++ projects/views.py
@@ -1,12 +1,13 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .serializers import ProjectReadSerializer, ProjectWriteSerializer
 from timey.permissions import IsOwnerOrAdminOrReadOnly
-from rest_framework import viewsets
 from .models import Project
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
-    permission_classes = [IsOwnerOrAdminOrReadOnly]
+    permission_classes = [IsAuthenticated, IsOwnerOrAdminOrReadOnly]
 
     def get_serializer_class(self):
         if self.request.method in ['POST', 'PUT', 'PATCH']:
