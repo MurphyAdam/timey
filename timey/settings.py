6f31fad74f9da7b771c7be304c9a66595966e8c1
Adam Clark <elm.majidi@gmail.com>
2021-10-20 15:41:12 +0100

    added DEFAULT_AUTHENTICATION_CLASSES to REST_FRAMEWORK settings

--- timey/settings.py
+++ timey/settings.py
@@ -95,6 +95,10 @@ WSGI_APPLICATION = 'timey.wsgi.application'
 # DRF settings
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
